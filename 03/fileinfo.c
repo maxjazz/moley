5a3958b0755529d131bf50a930dc5d70e01c4782
Maksim Prokopev <maksim.prokopev@jambook.ru>
2016-04-21 17:55:00 +0300

    Added mode_to_letters function

--- 03/fileinfo.c
+++ 03/fileinfo.c
@@ -8,8 +8,10 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <string.h>
 
 void show_stat(char *, struct stat* );
+void mode_to_letters (int, char);
 
 int main (int argc, char* argv[])
 {
@@ -39,4 +41,21 @@ void show_stat(char *fname, struct stat *buf)
   printf ("size: %lld\n", buf->st_size);
   printf ("modtime: %ld\n", buf->st_mtime);
   printf ("name: %s\n", fname);
+}
+
+void mode_to_letter(int mode, char str[])
+{
+  strcpy (str,"----------");
+  if (S_ISDIR(mode)) str[0]='d';
+  if (S_ISCHR(mode)) str[0]='c';
+  if (S_ISBLK(mode)) str[0]='b';
+  if (mode & S_IRUSR) str[1] = 'r';
+  if (mode & S_IWUSR) str[2] = 'w';
+  if (mode & S_IXUSR) str[3] = 'x';
+  if (mode & S_IRGRP) str[4] = 'r';
+  if (mode & S_IWGRP) str[5] = 'w';
+  if (mode & S_IXGRP) str[6] = 'x';
+  if (mode & S_IROTH) str[7] = 'r';
+  if (mode & S_IWOTH) str[8] = 'w';
+  if (mode & S_IXOTH) str[9] = 'x';
 }
