ca4cb15d2ffaa32c6f09888f9712a7cbc9f62f7b
Maksim Prokopev <maksim.prokopev@jambook.ru>
2016-04-22 11:38:17 +0300

    Added mode_to_letters function

--- 03/ls.c
+++ 03/ls.c
@@ -9,6 +9,7 @@ ls - show directory content
 #include <dirent.h>
 
 void do_ls(char []);
+void mode_to_letters(int, char []);
 
 int main (int argc, char *argv[])
 
@@ -36,4 +37,21 @@ void do_ls (char dirname[])
         printf("%s\n", direntp->d_name );
     }
   closedir(dir_ptr);
+}
+
+void mode_to_letter(int mode, char str[])
+{
+  strcpy (str,"----------");
+  if (S_ISDIR(mode)) str[0]='d';
+  if (S_ISCHR(mode)) str[0]='c';
+  if (S_ISBLK(mode)) str[0]='b';
+  if (mode & S_IRUSR) str[1] = 'r';
+  if (mode & S_IWUSR) str[2] = 'w';
+  if (mode & S_IXUSR) str[3] = 'x';
+  if (mode & S_IRGRP) str[4] = 'r';
+  if (mode & S_IWGRP) str[5] = 'w';
+  if (mode & S_IXGRP) str[6] = 'x';
+  if (mode & S_IROTH) str[7] = 'r';
+  if (mode & S_IWOTH) str[8] = 'w';
+  if (mode & S_IXOTH) str[9] = 'x';
 }
