18ba5e318ac6fed53959cba2fe9b9c92f3fc3a06
Maksim Prokopev <maksim.prokopev@jambook.ru>
2016-04-25 15:29:40 +0300

    Added support for Set-User-ID and Set-Group-ID

--- 03/ls.c
+++ 03/ls.c
@@ -138,9 +138,11 @@ void mode_to_string(int mode, char str[])
   if (mode & S_IRUSR) str[1] = 'r';
   if (mode & S_IWUSR) str[2] = 'w';
   if (mode & S_IXUSR) str[3] = 'x';
+  if (mode & S_ISUID) str[3] = 's';
   if (mode & S_IRGRP) str[4] = 'r';
   if (mode & S_IWGRP) str[5] = 'w';
   if (mode & S_IXGRP) str[6] = 'x';
+  if (mode & S_ISGID) str[6] = 's';
   if (mode & S_IROTH) str[7] = 'r';
   if (mode & S_IWOTH) str[8] = 'w';
   if (mode & S_IXOTH) str[9] = 'x';
