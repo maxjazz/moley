13ebd6c428d954402b6e02ee21d9a63c3227eee4
Maksim Prokopev <maksim.prokopev@jambook.ru>
2016-05-04 14:19:30 +0300

    Disabled bufferisation and echo on terminal

--- 06/icanon.sh
+++ 06/icanon.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 stty -icanon;
-./rotate
+./playagain
--- 06/playagain.c
+++ 06/playagain.c
@@ -4,27 +4,58 @@
 #define QUESTION "Do you want another transaction?"
 
 int get_response(char *);
+int tty_mode(int);
+void set_crmode();
 
 int main(int argc, char* argv[])
 {
   int response;
+  tty_mode(0);
+  set_crmode();
   response = get_response(QUESTION);
+  tty_mode(1);
   return response;
 }
 
 int get_response (char *question)
 {
+  int input;
   printf("%s (y/n)\n", question );
   while (1)
   {
-    switch(getchar())
+    switch(input = getchar())
     {
       case 'y':
       case 'Y': return 0;
       case 'n':
       case 'N':
       case EOF: return 1;
+      default:
+        printf("Cannot understand %c\n", input );
+        printf("Please, type y or n\n");
     }
   }
 
+}
+
+void set_crmode()
+{
+  struct termios ttystate;
+  tcgetattr(0, &ttystate);
+  ttystate.c_lflag &= ~ICANON;
+  ttystate.c_lflag &= ~ECHO;
+  ttystate.c_cc[VMIN]=1;
+  tcsetattr(0,TCSANOW,&ttystate);
+}
+
+int tty_mode(int how)
+{
+  static struct termios original_mode;
+  if (how == 0)
+  {
+    tcgetattr(0, &original_mode);
+    return 0;
+  }
+  else
+    return tcsetattr(0,TCSANOW, &original_mode);
 }
